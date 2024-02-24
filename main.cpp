3d48bd802601137bb9c66a389c5dccb02b99fd82
deltakernel <obscuroart@gmail.com>
2024-02-24 11:19:46 -0300

    Create README.md

--- main.cpp
+++ main.cpp
@@ -6,7 +6,24 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     Notepad notepad;
-    notepad.show();
+    
+
+    QMap<int, QString> commands ;
+    
+    for (int i = 0; i < argc; i++)
+    {
+        commands[i] = argv[i];
+    }
+
+    int index = commands.key("-d");//args.indexOf("-o");
+
+    if(index  < (argc-1)){
+        qDebug() << "args[" << index << "]= " << commands[index+1];
+        notepad.show();
+    } else {
+        qDebug() << "Digite o comando corretamente\no especificador " << commands[index] << "deve receber um argumento"; 
+        notepad.close();
+    }
 
     return app.exec();
 }
